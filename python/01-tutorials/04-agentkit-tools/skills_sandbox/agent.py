a6d1dea2e7180232566c057402b0db084bccaf3a
warm-wm <wangmeng.823@bytedance.com>
2026-01-28 18:41:36 +0800

    feat: support byteplus

--- python/01-tutorials/04-agentkit-tools/skills_sandbox/agent.py
+++ python/01-tutorials/04-agentkit-tools/skills_sandbox/agent.py
@@ -2,39 +2,28 @@ from agentkit.apps import AgentkitAgentServerApp
 from veadk import Agent, Runner
 from veadk.memory.short_term_memory import ShortTermMemory
 from veadk.tools.builtin_tools.execute_skills import execute_skills
+from prompts.prompt import ROOT_AGENT_INSTRUCTION_CN, ROOT_AGENT_INSTRUCTION_EN
 import os
 
 app_name = "agent_skills_app"
 user_id = "agent_skills_user"
 session_id = "agent_skills_session"
 
+ROOT_AGENT_INSTRUCTION = ROOT_AGENT_INSTRUCTION_CN
+
+provider = os.getenv("CLOUD_PROVIDER")
+if provider and provider.lower() == "byteplus":
+    ROOT_AGENT_INSTRUCTION = ROOT_AGENT_INSTRUCTION_EN
+
 skill_space_id = os.getenv("SKILL_SPACE_ID")
 agent = Agent(
     name="skill_agent",
-    instruction="根据用户的需求，调用 execute_skills 工具执行 skills，",
+    instruction=ROOT_AGENT_INSTRUCTION,
     skills=[skill_space_id],
     tools=[execute_skills],
 )
 
 short_term_memory = ShortTermMemory(backend="local")
-
-runner = Runner(
-    agent=agent,
-    short_term_memory=short_term_memory,
-    app_name=app_name,
-    user_id=user_id,
-)
-
-
-async def main():
-    messages = """
-    请运行以下工作流程：
-    1. 帮我写一个pdf处理的skill，能够支持加载pdf、编辑pdf和从pdf中提取文字信息即可。
-    2. 将写好的 skill 注册到 skill space。
-    """
-    response = await runner.run(messages=messages, session_id=session_id)
-    print(f"response: {response}")
-
 
 # using veadk web for debugging
 root_agent = agent
@@ -45,6 +34,4 @@ agent_server_app = AgentkitAgentServerApp(
 )
 
 if __name__ == "__main__":
-    # import asyncio
-    # asyncio.run(main())
     agent_server_app.run(host="0.0.0.0", port=8000)
---
+++ python/01-tutorials/04-agentkit-tools/skills_sandbox/prompts/prompt.py
@@ -0,0 +1,7 @@
+ROOT_AGENT_INSTRUCTION_CN = """
+根据用户的需求，调用 execute_skills 工具执行 skills。
+"""
+
+ROOT_AGENT_INSTRUCTION_EN = """
+Based on the user's requirements, call the execute_skills tool to execute skills.
+"""
